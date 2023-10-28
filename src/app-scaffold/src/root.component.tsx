8a2af4d3f4f6418ee3f5238c8606a55b2f5d7647
Murilo Beltrame <murilobeltrame@gmail.com>
2023-10-28 10:40:46 -0300

    ...

--- src/app-scaffold/src/root.component.tsx
+++ src/app-scaffold/src/root.component.tsx
@@ -1,3 +1,13 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+import Parcel from "single-spa-react/parcel"
+import * as countriesConfig from "@wineyard/countries"
+
+export const Root=({name}) => {
+  return (
+    <>
+      <h1>{name}</h1>
+      <Parcel config={countriesConfig} />
+    </>
+  )
 }
+
+export default Root

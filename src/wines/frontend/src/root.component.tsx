0dd4f83e323a73cdc21b8e52e3a95d61efa02324
Murilo Beltrame <murilobeltrame@gmail.com>
2023-11-02 22:57:25 -0300

    ...

--- src/wines/frontend/src/root.component.tsx
+++ src/wines/frontend/src/root.component.tsx
@@ -4,30 +4,35 @@ export default function Root() {
   const [selectedCountries, setSelectedCountries] = useState<string[]>([])
   const [selectedGrapes, setSelectedGrapes] = useState<string[]>([])
 
-  useEffect(() => window.addEventListener('@wineyard/countries/selected', event => {
+  window.addEventListener('@wineyard/countries/selected', event => {
     console.log('Received Countries/Selected into Wines')
-    console.log(event)
-    // const e:any = event;
-    // console.log(selectedCountries)
-    // const n = [...selectedCountries, e.detail.name]
-    // console.log(n)
-    // setSelectedCountries(n)
-  }), [])
+    const e:any = event;
+    setSelectedCountries([...selectedCountries, e.detail.name])
+  })
 
-  useEffect(() => window.addEventListener('@wineyard/countries/unselected', event => {
+  window.addEventListener('@wineyard/countries/unselected', event => {
     console.log('Received Countries/Unselected into Wines')
-    console.log(event)
-  }), [])
+    const e:any = event;
+    const n = selectedCountries
+    const idx = n.indexOf(e.detail.name)
+    n.splice(idx, 1)
+    setSelectedCountries([...n])
+  })
 
-  useEffect(() => window.addEventListener('@wineyard/grapes/selected', event => {
+  window.addEventListener('@wineyard/grapes/selected', event => {
     console.log('Received Grapes/Selected into Wines')
-    console.log(event)
-  }), [])
+    const e:any = event;
+    setSelectedGrapes([...selectedGrapes, e.detail.name])
+  })
 
-  useEffect(() => window.addEventListener('@wineyard/grapes/unselected', event => {
+  window.addEventListener('@wineyard/grapes/unselected', event => {
     console.log('Received Grapes/Unselected into Wines')
-    console.log(event)
-  }), [])
+    const e:any = event;
+    const n = selectedGrapes
+    const idx = n.indexOf(e.detail.name)
+    n.splice(idx, 1)
+    setSelectedGrapes([...n])
+  })
 
   return (
     <>
